981ebd3d79d3b0c9837a3635b0eb48942d40a0a1
GABER77 <mg7965724@gmail.com>
2025-04-29 22:02:16 +0300

    Items grid layout for collection screen

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,10 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/getAssets";
+import Title from "../components/Title";
+import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(() => {
+    setFilterProducts(products);
+  }, [products]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-7 border-t">
@@ -40,6 +47,35 @@ const Collection = () => {
               </label>
             ))}
           </div>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"Collections"} />
+
+          {/* Product Sort */}
+          <select className="border-2 border-gray-500 text-sm px-2">
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+
+        {/* Products Grid */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+          {filterProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              images={item.images}
+              name={item.name}
+              price={item.price}
+              onSale={item.onSale}
+              category={item.category}
+            />
+          ))}
         </div>
       </div>
     </div>
